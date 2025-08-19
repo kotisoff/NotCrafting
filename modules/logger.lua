3946e3ddd8b6ae5d809c39910952a03464709432
KotIsOff <kot252steam@gmail.com>
2025-08-20 00:27:07 +0300

    фикс логов

--- modules/logger.lua
+++ modules/logger.lua
@@ -1,19 +1,3 @@
-local _logger = require "shared/utils/not_utils".Logger.new("not_crafting");
-
-local logger = {
-  levels = _logger.levels,
-  clear = function() _logger:clear() end,
-
-  ---@param level not_utils.logger.levels
-  log = function(level, ...)
-    _logger:log(level, ...);
-  end,
-  print = function()
-    _logger:print()
-  end,
-  println = function(level, ...)
-    _logger:println(level, ...);
-  end
-}
+local logger = require "shared/utils/not_utils".Logger.new("not_crafting");
 
 return logger;
